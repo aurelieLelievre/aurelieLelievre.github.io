82cc472447c328755e4a6c06c00d529255dd94e3
Aurélie Le Lièvre <aurelie.lelievre1@gmail.com>
2017-10-18 11:59:38 +0200

    Commit 1

---
+++ js/app.js
@@ -0,0 +1,44 @@
+var app = angular.module('myApp', ['ngSanitize']);
+
+app.controller('myCtrl', function($scope, $http, $location, $sce) {
+    
+    
+    $scope.init = function(page){
+        
+        $scope.changeLanguage('fr');
+        $scope.page=$location.path().replace("/", "");
+        
+        if ($location.path()===""){
+            $scope.page="CV";
+        }
+        
+    };
+   
+    $scope.getData = function(lang){
+        $http.get("ressources/CV-"+lang+".json").success(function(response) {
+            $scope.cv = response.CV;
+        });
+        $http.get("ressources/portal-"+lang+".json").success(function(response) {
+            $scope.portal = response;
+        });
+        $http.get("ressources/contact-"+lang+".json").success(function(response) {
+            $scope.contact = response;
+        });
+        
+    }
+
+    $scope.changeLanguage = function(language){
+        $scope.getData(language);
+    };
+    
+    $scope.changePage = function(page){
+        $location.path(page);
+        $scope.page=page;
+    };
+    
+    $scope.trustHTML = function(text){
+        return($sce.trustAsHtml(text));
+    };
+    
+});
+
